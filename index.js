222247e91e8183a3c2ccfe9ef0cb2e1b5f898e10
[sterrevangeest] <[sterrevangeest@hotmail.nl]>
2019-03-29 14:26:23 +0100

    update

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var init = {
     var app = express();
     var port = process.env.PORT || 3000;
 
-    app.listen(port);
+    app.listen(process.env.PORT || 3000);
 
     require("dotenv").config();
 
